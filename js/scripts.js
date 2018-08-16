1aca26a2ace45b9d96d576a6ad77b84fc739974e
Meria Thomas and Mel Yasuda <mel@>
2018-08-16 13:25:05 -0700

    input string and replace all punctuation using replace and convert into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
+var array = function(input){
+  var array = input.split("");
+  alert(array);
+}
+
 $(document).ready(function(){
   $("#input").submit(function(event){
     event.preventDefault();
 
-    var input = $("#sentence").val();
-
-    $("#result").append(input);
+    var input = ($("#sentence").val()).trim();
+    input = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    $("#result").append(array(input));
   });
 });
